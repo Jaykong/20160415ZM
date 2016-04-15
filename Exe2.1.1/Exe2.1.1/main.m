314613ec17eba788aa9a2c54c8f1bdccc4f444fa
zhangmiao94520 <495063555@qq.com>
2016-04-15 15:46:04 +0800

    Exe2.1.1

---
+++ Exe2.1.1/Exe2.1.1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Exe2.1.1
+//
+//  Created by 张苗 on 4/15/16.
+//  Copyright © 2016 张苗. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+int main (int argc,const char *argv[]){
+    @autoreleasepool {
+        
+        NSLog(@"HELLO");
+    }
+    return 0;
+}
