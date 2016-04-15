5f9c05d27c633f44b96ee7e85d4c089043f42b66
zhangmiao94520 <495063555@qq.com>
2016-04-15 17:11:30 +0800

    stackView

---
+++ inClass/StackView/StackView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackView
+//
+//  Created by 张苗 on 4/15/16.
+//  Copyright © 2016 张苗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
